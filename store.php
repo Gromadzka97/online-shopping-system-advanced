539d7ed15dbeecb0d0be9561231a654463ccdbde
Michał Spyra <ennduo@icloud.com>
2021-05-27 20:07:11 +0200

    fixed possibility of entering e letter in number inputs

--- store.php
+++ store.php
@@ -56,13 +56,13 @@ include 'header.php';
 							<div class="price-filter">
 								<div id="price-slider" class="noUi-target noUi-ltr noUi-horizontal"><div class="noUi-base"><div class="noUi-origin" style="left: 0%;"><div class="noUi-handle noUi-handle-lower" data-handle="0" tabindex="0" role="slider" aria-orientation="horizontal" aria-valuemin="0.0" aria-valuemax="100.0" aria-valuenow="0.0" aria-valuetext="1.00" style="z-index: 5;"></div></div><div class="noUi-connect" style="left: 0%; right: 0%;"></div><div class="noUi-origin" style="left: 100%;"><div class="noUi-handle noUi-handle-upper" data-handle="1" tabindex="0" role="slider" aria-orientation="horizontal" aria-valuemin="0.0" aria-valuemax="100.0" aria-valuenow="100.0" aria-valuetext="999.00" style="z-index: 4;"></div></div></div></div>
 								<div class="input-number price-min">
-									<input id="price-min" type="number">
+									<input id="price-min" onkeydown="javascript: return event.keyCode == 69 | 189 ? false : true" type="number">
 									<span class="qty-up">+</span>
 									<span class="qty-down">-</span>
 								</div>
 								<span>-</span>
 								<div class="input-number price-max">
-									<input id="price-max" type="number">
+									<input id="price-max" onkeydown="javascript: return event.keyCode == 69 | 189 ? false : true" type="number">
 									<span class="qty-up">+</span>
 									<span class="qty-down">-</span>
 								</div>
